d45be7908c0390b5a78ec13ddeebd6769fbfcdbd
Anna Tuchina <a.tuchina.anna@yandex.com>
2024-10-12 09:15:01 +0300

    sqrt with iterations

--- labs/number2.cpp
+++ labs/number2.cpp
@@ -12,11 +12,13 @@ int main()
         double sqr_rt = sqrt(2);
         double sqr_rt_1 = 0.1;
         double tp = 2.;
+        int i = 0;
         while (abs(sqr_rt - 1 - (1/sqr_rt_1)) > eps) {
             sqr_rt_1 = 2 + 1/tp;
             tp = sqr_rt_1;
+            i++;
         }
-        cout << setprecision(10) << setw(12) << (1/sqr_rt_1) + 1 << "(accuracy = " << eps << ")" << endl;
+        cout << setprecision(10) << setw(12) << (1/sqr_rt_1) + 1 << "(accuracy = " << eps << ")" << " i = " << i << endl;
     }
     return 0;
 }
